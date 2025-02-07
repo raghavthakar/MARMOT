127d6d34bad7d3679b1a6be9a2c4f7c998bbaccd
Raghav <raghavthakar12@gmail.com>
2025-02-06 17:14:33 -0800

    ea only experiences for training

--- ea_exp_rl.py
+++ ea_exp_rl.py
@@ -17,30 +17,25 @@ torch.manual_seed(2024)
 random.seed(2024)
 
 interface = MORoverInterface.MORoverInterface("config/MORoverEnvConfig.yaml")
-td3 = TD3(state_dim=12, action_dim=interface.get_action_size(), max_action=1.0, num_heads=1)
-rep_buffs = [ReplayBuffer(state_dim=12, action_dim=interface.get_action_size(), max_size=50_000) for _ in range(1)]
+td3 = TD3(state_dim=12, action_dim=interface.get_action_size(), max_action=1.0, num_heads=2, num_critics=2)
+rep_buffs = [ReplayBuffer(state_dim=12, action_dim=interface.get_action_size(), max_size=50_000) for _ in range(2)]
 nsga = nsgaii.NSGAII(alg_config_filename="config/MARMOTConfig.yaml", rover_config_filename="config/MORoverEnvConfig.yaml", 
                                 replay_buffers=rep_buffs)
 
-for i in range(120):
-    # traj, g = interface.rollout(td3.actor, [0], noisy_action=True, noise_std=0.15)
-    # add_experiences_to_replay_buffers(traj, rep_buffs=rep_buffs, active_agents_indices=[0])
-    nsga.evolve()
-    traj, g = interface.rollout(td3.actor, [0], noisy_action=True, noise_std=0.15)
-    for transition in traj[0]:
-        rep_buffs[0].add(*rep_buffs[0].parse_transition_dict(transition))
+for i in range(1200):
+    pop, roster_wise_team_combs, roster_wise_team_fits, chosen_roster, champion_indices = nsga.evolve()
 
-    for _ in range(625):
-        td3.train(replay_buffer=rep_buffs[0], agent_id=0)
-    
-    
-
+    # if i > 10:
+    #     td3.set_new_actor(chosen_roster)
+    #     for _ in range(625):
+    #         td3.train(replay_buffer=rep_buffs[0], agent_id=0)
+    #         td3.train(replay_buffer=rep_buffs[1], agent_id=1)
 
     if(i % 5 == 0):
-        traj, g = interface.rollout(td3.actor, [0])
+        traj, g = interface.rollout(chosen_roster, [0, 1])
         print("Epoch:", i, "G:", g, "Final Location:", traj[0][-1]["location"])
 
 print("Finished Training")
 
-traj, g = interface.rollout(td3.actor, [0])
+traj, g = interface.rollout(td3.actor, [0, 1])
 print("G:", g)
